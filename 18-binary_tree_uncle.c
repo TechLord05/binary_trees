5657f2093577f3101c07062bb651d309ee99e72b
DevHeart1 <obasipeace412@yahoo.com>
2024-01-04 06:47:15 -0800

    error fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,17 +8,26 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent = node->parent->parent;
-
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
 
-	if (grandparent->left == node->parent)
+	binary_tree_t *parent = node->parent;
+	binary_tree_t *grandparent = parent->parent;
+
+	if (grandparent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (grandparent->left == parent)
+	{
 		return (grandparent->right);
-	else if (grandparent->right == node->parent)
+	}
+	else
+	{
 		return (grandparent->left);
-		
-	return (NULL);
+	}
 }
+
