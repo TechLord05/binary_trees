969f080ad48c04b7e4c68c1cd49cc5447c208fbb
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-01-04 02:55:08 +0000

    Binary tree tree implementation

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+    /*Recursively call (root)tree->left until it is NULL*/
+    binary_tree_delete(tree->left);
+    /*Recursively call (root)tree->right until it is NULL*/
+    binary_tree_delete(tree->right);
+    /*Free allocated memory*/
+    free(tree);
+}
